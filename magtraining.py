a91f605775035cb04af1a73d7f6d68d29f14229c
Miguvill <miguel.villanueva@myamazonguy.com>
2024-05-30 00:24:39 +0800

    Update magtraining.py

--- magtraining.py
+++ magtraining.py
@@ -1,9 +1,23 @@
+from flask import Flask
 from werkzeug.urls import url_quote_plus as url_quote
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/')
 def training():
-    #construct your message here
-
+    # Construct your message here
+    message = """
+    <html>
+        <head>
+            <title>Training Page</title>
+        </head>
+        <body>
+            <h1>Training 22</h1>
+            <p>Hi, my name is Miguel</p>
+        </body>
+    </html>
+    """
     return message
+
+if __name__ == '__main__':
+    app.run(debug=True)
